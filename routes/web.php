a92fdb71400cb7449cb6fb56b85e169cabf3a5c3
1 <1@qq.com>
2020-01-01 10:50:51 +0800

    邮箱验证功能

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,5 @@
 |
 */
 
-Route::get('/', 'PagesController@root')->name('root');
-Auth::routes();
+Route::get('/', 'PagesController@root')->name('root')->middleware('verified');
+Auth::routes(['verify' => true]);
