998542c6a11dee7dd0dcd2e28e88fa764dc5a38e
1 <1@qq.com>
2020-01-06 00:57:42 +0800

    用户界面 - 商品列表

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,8 @@
 */
 
 Route::get('/', 'PagesController@root')->name('root')->middleware('verified');
+Route::redirect('/', '/products')->name('root');
+Route::get('products', 'ProductsController@index')->name('products.index');
 Auth::routes(['verify' => true]);
 
 // auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
@@ -27,4 +29,5 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
 	Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
 	
 	Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
+	
 });
